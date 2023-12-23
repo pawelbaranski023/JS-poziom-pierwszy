00f009fa05e5d3766ada1714ba7c1ae02c59c89c
Pawel Baranski <pawelbaranski023@gmail.com>
2023-12-23 22:46:54 +0100

    lesson 29

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let ul;
+let shoppingList = [
+  "Mleko",
+  "Jajka",
+  "Jogurt",
+  "Pasztet",
+  "Bułka",
+  "Ogórki kiszone",
+];
 
 document.addEventListener("DOMContentLoaded", () => {
   ul = document.getElementById("shoppingList");
-  addListItem();
+
+  for (let shoppingProduct of shoppingList) {
+    addListItem(shoppingProduct);
+  }
 });
 
-function addListItem() {
+function addListItem(shoppingProduct) {
   let li = document.createElement("li");
-  li.innerText = "test";
+  li.innerText = shoppingProduct;
   ul.appendChild(li);
 }
